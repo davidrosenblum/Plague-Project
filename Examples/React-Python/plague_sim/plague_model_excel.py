77cac08fe4959f6c0c8198c9cc0687e804e4e972
Robert Juall <RJuall@gmail.com>
2018-10-08 17:31:17 -0400

    Implemented day 0 calc in static method

--- Examples/React-Python/plague_sim/plague_model_excel.py
+++ Examples/React-Python/plague_sim/plague_model_excel.py
@@ -1,24 +1,29 @@
 from decimal import *
 from .disease_model import DiseaseModel
+from .plague_params import PlagueParams
 
 class PlagueModelExcel(DiseaseModel):
     
     @staticmethod
     def calc_day_zero(self, plague_params):
-        pass
+        return { "Susceptible"     : plague_params.initial_pop - plague_params.initial_infected,
+                 "Infected"        : plague_params.initial_infected,
+                 "Immune"          : plague_params.initial_pop * plague_params.immune_percent,
+                 "Dead"            : 0,
+                 "TotalPopulation" : plague_params.initial_pop }
 
     @staticmethod
     def calc_susceptible(self, plague_params, prev_disease_day):
-        pass
+        return 
 
     @staticmethod
     def calc_immune(self, plague_params, prev_disease_day):
-        pass
+        return 0
 
     @staticmethod
     def calc_infected(self, plague_params, prev_disease_day):
-        pass
+        return 0
 
     @staticmethod
     def calc_dead(self, plague_params, prev_disease_day):
-        pass
+        return 0
