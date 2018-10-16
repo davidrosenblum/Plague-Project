b75af61a2bdd7a19ad5658f0fe122bb8827ffd0e
JT <jt@MSI.com>
2018-10-16 11:05:53 -0400

    Modal inital UI Little CSS/No Bootstrap

--- web/src/components/Modal.jsx
+++ web/src/components/Modal.jsx
@@ -2,12 +2,37 @@ import React from "react";
 import ReactModal from "react-modal"
 
 export class Modal extends React.Component{
+
+	onClick(){
+
+	}
+
 	render(){
 		return (
 			<div>
 				<ReactModal isOpen={this.props.showModal} >
-					My React Modal
-      				<a onClick={this.props.closeModal} href=""><span class="close">&times;</span></a>
+      				<a onClick={this.props.closeModal} href=""><span className="close">&times;</span></a>
+      					<h2 className="modalHeader">Report Bug or Request New Feature</h2>
+      					<table>
+      						<tr>
+	      						<td className="bugReport">
+	      							<h5>Report Bug:</h5>
+			      					<textarea rows="4" cols="100">
+			      						Report Bug Here
+			      					</textarea>
+			      					<br/>
+			      					<button onClick={this.onClick.bind(this)}>Submit</button>
+		      					</td>
+		      					<td className="featureRequest">
+		      						<h5>Request Feature:</h5>
+			      					<textarea rows="4" cols="100">
+			      						 Request feature Here
+			      					</textarea>
+			      					<br/>
+			      					<button onClick={this.onClick.bind(this)}>Submit</button>
+		      					</td>
+	      					</tr>
+      					</table>
       			</ReactModal>
 			</div>
 		);
