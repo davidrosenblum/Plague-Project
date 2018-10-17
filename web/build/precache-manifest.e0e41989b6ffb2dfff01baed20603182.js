0daba2cc6dac2fa1f7d290dea08a9427968c1bc2
David Rosenblum <david@rosenblum.net>
2018-10-17 10:48:42 -0400

    Fixed broken CSV download URL

---
+++ web/build/precache-manifest.e0e41989b6ffb2dfff01baed20603182.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e02f0514cb692ad1e88b",
+    "url": "/static/js/main.e02f0514.chunk.js"
+  },
+  {
+    "revision": "fc0458037c294c96898c",
+    "url": "/static/js/1.fc045803.chunk.js"
+  },
+  {
+    "revision": "e02f0514cb692ad1e88b",
+    "url": "/static/css/main.6260302c.chunk.css"
+  },
+  {
+    "revision": "88fcbbe7016178885aca015a30933ca7",
+    "url": "/index.html"
+  }
+];
