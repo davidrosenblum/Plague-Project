ff1a527bd25d8b9a5e9b660d7a2f0258e52f5476
Robert Juall <RJuall@gmail.com>
2018-10-17 23:31:30 -0400

    Implemented the plague model caluculations using the mpmath library

--- Examples/React-Python/plague_sim/plague_model_excel.py
+++ Examples/React-Python/plague_sim/plague_model_excel.py
@@ -1,21 +1,54 @@
-from mpmath import *
+from mpmath import mpf, mp
 from .disease_model import DiseaseModel
 from .plague_params import PlagueParams
 
 class PlagueModelExcel(DiseaseModel):
     
+    mp.dps = 8
+
     @staticmethod
     def calc_susceptible(self, plague_params, prev_disease_day):
-        return 0
+        return str(
+            mpf(prev_disease_day["Susceptible"])  -  \
+           (mpf(plague_params.virility)           *  \
+            mpf(prev_disease_day["Susceptible"])  *  \
+            mpf(prev_disease_day["Infected"])     /  \
+            mpf(plague_params.initial_pop)
+           )
+        )
 
     @staticmethod
     def calc_immune(self, plague_params, prev_disease_day):
-        return 0
+        return str(
+            mpf(prev_disease_day["Immune"])       +  \
+           (mpf(prev_disease_day["Infected"])     /  \
+            mpf(plague_params.infection_length)
+           )
+        )
 
     @staticmethod
     def calc_infected(self, plague_params, prev_disease_day):
-        return 0
+        return str(
+            mpf(prev_disease_day["Infected"])     +  \
+           (mpf(plague_params.virility)           *  \
+            mpf(prev_disease_day["Susceptible"])  *  \
+            mpf(prev_disease_day["Infected"])     /  \
+            mpf(plague_params.initial_pop)        -  \
+            mpf(prev_disease_day["Infected"])     /  \
+            mpf(plague_params.infection_length)   -  \
+            mpf(prev_disease_day["Infected"])     *  \
+            mpf(plague_params.percent_fatal)      /  \
+           (mpf(100)                              *  \
+            mpf(plague_params.infection_length))
+           )
+        )
 
     @staticmethod
     def calc_dead(self, plague_params, prev_disease_day):
-        return 0
+        return str(
+            mpf(plague_params.percent_fatal)      *  \
+            mpf(prev_disease_day["Infected"])     /  \
+           (mpf(100)                              *  \
+            mpf(plague_params.infection_length))  +  \
+            mpf(prev_disease_day["Dead"])
+        )
