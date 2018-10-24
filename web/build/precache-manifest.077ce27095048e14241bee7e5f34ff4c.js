0feb10fede4402aadcc6da418e9e9f6276cb2e22
David Rosenblum <david@rosenblum.net>
2018-10-24 11:57:16 -0400

    Rebuild + requirements fix

---
+++ web/build/precache-manifest.077ce27095048e14241bee7e5f34ff4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5aeb05d870986bc8dc67",
+    "url": "/static/js/main.5aeb05d8.chunk.js"
+  },
+  {
+    "revision": "ec2c200d5efc6030f725",
+    "url": "/static/js/1.ec2c200d.chunk.js"
+  },
+  {
+    "revision": "5aeb05d870986bc8dc67",
+    "url": "/static/css/main.72ff84ce.chunk.css"
+  },
+  {
+    "revision": "f9c47b1660bcd1a3ce68bef89258f552",
+    "url": "/index.html"
+  }
+];
