148eaa64f29e3d3bc2c3b3f860aed9f60e4768fd
JT <jt@MSI.com>
2018-10-26 10:46:22 -0400

    Graph X & Y axis fix

--- web/build/precache-manifest.94976263ba113267b9e54ff5f1b5fefb.js
+++ web/build/precache-manifest.94976263ba113267b9e54ff5f1b5fefb.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "fd470d6421f9e791cd8d",
-    "url": "/static/js/main.fd470d64.chunk.js"
+    "revision": "1b324a28c926cdac2c08",
+    "url": "/static/js/main.1b324a28.chunk.js"
   },
   {
     "revision": "ec2c200d5efc6030f725",
     "url": "/static/js/1.ec2c200d.chunk.js"
   },
   {
-    "revision": "fd470d6421f9e791cd8d",
-    "url": "/static/css/main.72ff84ce.chunk.css"
+    "revision": "1b324a28c926cdac2c08",
+    "url": "/static/css/main.f491a99f.chunk.css"
   },
   {
-    "revision": "b89c45fa09e902ac762ed313094ae927",
+    "revision": "7a5038f7d913d614901a22a490307f3e",
     "url": "/index.html"
   }
 ];
