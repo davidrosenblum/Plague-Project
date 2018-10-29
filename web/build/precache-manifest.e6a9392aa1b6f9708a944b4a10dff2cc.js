6b4e273d58ccd91df828ba058b0fbe434267ac96
David Rosenblum <david@rosenblum.net>
2018-10-29 19:05:12 -0400

    Dynamic graph Y label

--- web/build/precache-manifest.e6a9392aa1b6f9708a944b4a10dff2cc.js
+++ web/build/precache-manifest.e6a9392aa1b6f9708a944b4a10dff2cc.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "955a16b5fd2d4cfc4c52",
-    "url": "/static/js/main.955a16b5.chunk.js"
+    "revision": "62cbe17e66807dbdb02b",
+    "url": "/static/js/main.62cbe17e.chunk.js"
   },
   {
     "revision": "ec2c200d5efc6030f725",
     "url": "/static/js/1.ec2c200d.chunk.js"
   },
   {
-    "revision": "955a16b5fd2d4cfc4c52",
+    "revision": "62cbe17e66807dbdb02b",
     "url": "/static/css/main.7ac78d14.chunk.css"
   },
   {
-    "revision": "222e74b8b5b4309db21d236f1f58977a",
+    "revision": "ea77154af1fb33badb1973b1dcc7904f",
     "url": "/index.html"
   }
 ];
