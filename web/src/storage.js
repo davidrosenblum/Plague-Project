74af2183b3d6fef8995cc0bf0444f99867230825
Robert Juall <RJuall@gmail.com>
2018-10-31 20:08:52 -0400

    Begun coding on ParameterStorage class

---
+++ web/src/storage.js
@@ -0,0 +1,25 @@
+// Session Storage class for storing
+//  parameter values that were rendered
+export class ParameterStorage {
+    constructor() {
+        this._paramCount = 0;
+        this._paramSet = {};
+        sessionStorage.setItem(this._paramCount, this._paramSet);
+    }
+
+    addStorage(infectionLength, transmissionRate, virulence,
+        initPop, initImmune, initInfected, simLength)
+    {
+        this._paramCount++;
+        this._paramSet = {
+            "InfectionLength" : "",
+            "TransmissionRate": "",
+            "Virulence"       : "",
+            "InitialPop"      : "",
+            "InitialImmune"   : "",
+            "InitialInfected" : "",
+            "SimLength"       : "",
+        };
+        sessionStorage.setItem(this._paramCount, this._paramSet);
+    }
+}
