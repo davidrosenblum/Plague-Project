b6c352b20d9e2b9e00fc3841d904ba6689a7242a
David Rosenblum <david@rosenblum.net>
2018-11-15 11:54:51 -0500

    Tornado static files

--- server/handlers/main_handler.py
+++
@@ -1,8 +0,0 @@
-import tornado.web
-
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        # send index file
-        with open("./web/build/index.html") as file:
-            self.finish(file.read())
