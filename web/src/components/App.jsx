a5fd11a8ba8bd33d955fd4de0aecb3f20507409c
Robert Juall <RJuall@gmail.com>
2018-11-19 15:18:56 -0500

    Import print.css

--- web/src/components/App.jsx
+++ web/src/components/App.jsx
@@ -1,5 +1,6 @@
 import "../lib/bootstrap.min.css";
 import "./App.css";
+import "./Print.css";
 import React from "react";
 import { Container, Row, Col, Card, CardBody } from "reactstrap";
 import { Inputs } from "./Inputs";
